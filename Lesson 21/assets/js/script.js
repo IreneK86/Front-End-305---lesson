e7f0f531a21a96787fa02e8333f83977d64d6e61
IreneK86 <angelsewmoss@gmail.com>
2022-07-18 21:03:21 +0200

    Created new folder

---
+++ Lesson 21/assets/js/script.js
@@ -0,0 +1,11 @@
+let name = prompt('Tell me your name, please')
+console.log(name)
+
+let numberOne = prompt('type 10')
+let numberTwo = prompt('type 10') 
+
+let z = parseInt(numberOne) + parseInt(numberTwo)
+console.log(z)
+
+alert('Good job!')
+
