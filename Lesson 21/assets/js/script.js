00d22912ff9a89c1c9337362b7d896d6d8c4ce54
IreneK86 <angelsewmoss@gmail.com>
2022-07-21 19:57:56 +0200

    added task

--- Lesson 21/assets/js/script.js
+++ Lesson 21/assets/js/script.js
@@ -1,44 +1,51 @@
-let name = prompt('Tell me your name, please');
-console.log(name);
+// let name = prompt('Tell me your name, please');
+// console.log(name);
 
-let numberOne = prompt('type 10');
-let numberTwo = prompt('type 10');
-let z = parseInt(numberOne) + parseInt(numberTwo);
-console.log(z);
+// let numberOne = prompt('type 10');
+// let numberTwo = prompt('type 10');
+// let z = parseInt(numberOne) + parseInt(numberTwo);
+// console.log(z);
 
-alert('Good job!');
+// alert('Good job!');
 
-let userName = prompt('Enter your name: ');
-console.log(userName);
-alert('Your name is ' + userName);
+// let userName = prompt('Enter your name: ');
+// console.log(userName);
+// alert('Your name is ' + userName);
 
-let birthDate = prompt('Enter your year of birth ');
-const nowDate = 2022;
-let userAge = nowDate - parseInt(birthDate);
-console.log(userAge);
-alert('You are ' + userAge + ' this year');
+// let birthDate = prompt('Enter your year of birth ');
+// const NOWDATE = 2022;
+// let userAge = NOWDATE - parseInt(birthDate);
+// console.log(userAge);
+// alert('You are ' + userAge + ' this year');
 
-let sideOfSquareLength = prompt('Enter the length of a square in meters');
-let perimeterSquare = parseInt(sideOfSquareLength) * parseInt(sideOfSquareLength);
-console.log(perimeterSquare);
-alert('Perimeter of the Square is ' +  perimeterSquare + ' meters' );
+//let sideOfSquareLength = prompt('Enter the length of a square in meters');
+//let perimeterSquare = parseInt(sideOfSquareLength) * 4;
+//console.log(perimeterSquare);
+//alert('Perimeter of the Square is ' +  perimeterSquare + ' meters' );
 
-let radiusOfCircle = prompt('Enter the radius of a circle');
-let areaOfCircle = 3.14 * parseInt(radiusOfCircle) * parseInt(radiusOfCircle);
-console.log(areaOfCircle);
-alert('Area of the Circle is ' + areaOfCircle);
+//let radiusOfCircle = prompt('Enter the radius of a circle');
+//const PI = 3.14159;
+//let areaOfCircle = PI * parseInt(radiusOfCircle) * parseInt(radiusOfCircle);
+//console.log(areaOfCircle);
+//alert('Area of the Circle is ' + areaOfCircle);
 
-let distance = prompt('Enter the distance in km between the two cities (km)');
-let time = prompt('Enter the time you want to cover it within (h)');
-let speed = parseInt(distance) / parseInt(time);
-console.log(speed);
-alert('Result is ' + speed + ' km/hour');
+//let distance = prompt('Enter the distance in km between the two cities (km)');
+//let time = prompt('Enter the time you want to cover it within (h)');
+//let speed = parseInt(distance) / parseInt(time);
+//console.log(speed);
+//alert('Result is ' + speed + ' km/hour');
 
-let usdCurrency = prompt('Enter some sum $: ');
-const exchangeRate = 0.98;
-let currencyConverter = exchangeRate * parseInt(usdCurrency);
-console.log(currencyConverter);
-alert('You will get ' + currencyConverter + ' EUR');
+//let usdCurrency = prompt('Enter some sum $: ');
+//const EXCHANGERATE = 0.98;
+//let currencyConverter = EXCHANGERATE * parseInt(usdCurrency);
+//console.log(currencyConverter);
+//alert('You will get ' + currencyConverter + ' EUR');
+
+//let wallet = prompt('Enter the sum');
+//let chocoladePrice = prompt('Price of a bar');
+//let numberOfChocolades = parseInt(wallet) / parseInt(chocoladePrice);
+//let moneyRemain = parseInt(wallet) - parseInt(umberOfChocolades) * parseInt(hocoladePrice);
+//alert('You have '+ numberOfChocolades + 'and your remaning money is ' + oneyRemain);
 
 
 
